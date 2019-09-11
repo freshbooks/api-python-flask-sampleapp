1276cb87a8c7b3cd42556c2f8b54496abe269001
Aradhya Kunwar <aradhya.kunwar@mail.utoronto.ca>
2019-09-11 15:32:00 -0400

    single endpoint to confirm oauth, app uses env variables See VI-1057

--- app.py
+++ app.py
@@ -1,31 +1,35 @@
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 import requests
 from flask import Flask, request
 import json
 
 app = Flask(__name__)
 
-session = requests.Session()
+BASE_URL = "https://api.freshbooks.com"
 
-@app.route("/OauthSession")
-def OauthSession():
-    session.headers.clear()
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+@app.route("/TestAuthentication")
+def oauth_session():
     auth_code = request.args.get('code')
-    auth_data = { "grant_type": "authorization_code",
-    "client_secret": "",
-    "code": auth_code,
-    "client_id": "",
-    "redirect_uri": ""}
-    auth_request = session.post("https://api.freshbooks.com/auth/oauth/token", json = auth_data)
+    auth_data = { 
+        "grant_type": "authorization_code",
+        "client_secret": os.getenv('CLIENT_SECRET'),
+        "code": auth_code,
+        "client_id": os.getenv('CLIENT_ID'),
+        "redirect_uri": os.getenv('REDIRECT_URL')
+    }
+    auth_request = requests.post(BASE_URL + "/auth/oauth/token", json = auth_data)
     access_token_dict = json.loads(auth_request.text)
-    session.headers.update({"Authorization":"Bearer "+ access_token_dict["access_token"]})
-    return access_token_dict
-
-@app.route("/Aboutme")
-def Aboutme():
-    identity_request = session.get("https://api.freshbooks.com/auth/api/v1/users/me")
+    headers = {"Authorization":"Bearer " + access_token_dict["access_token"]}
+    identity_request = requests.get(BASE_URL + "/auth/api/v1/users/me", headers=headers)
     identity_dict = json.loads(identity_request.text)
-    print(identity_dict)
-    return identity_dict
+    profile_dict = identity_dict["response"]["profile"]
+    response = "You have successfully logged in for " + profile_dict["first_name"] + " " + profile_dict["last_name"]
+    return response
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
