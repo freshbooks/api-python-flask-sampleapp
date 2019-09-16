8ec14120c4f664891ebb6cd417b49897fe62233a
Andrew McIntosh <nacho.vonkickbutt@gmail.com>
2019-09-16 07:13:07 -0400

    test

--- app.py
+++ app.py
@@ -1,35 +1,41 @@
 import os
-from os.path import join, dirname
+from os import path
 from dotenv import load_dotenv
-import requests
 from flask import Flask, request
-import json
+import requests
 
 app = Flask(__name__)
 
+
 BASE_URL = "https://api.freshbooks.com"
 
-dotenv_path = join(dirname(__file__), '.env')
+dotenv_path = path.join(path.dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
 
 @app.route("/TestAuthentication")
 def test_authentication():
     auth_code = request.args.get('code')
-    auth_data = { 
+    auth_data = {
         "grant_type": "authorization_code",
         "client_secret": os.getenv('CLIENT_SECRET'),
         "code": auth_code,
         "client_id": os.getenv('CLIENT_ID'),
         "redirect_uri": os.getenv('REDIRECT_URL')
     }
-    auth_request = requests.post(BASE_URL + "/auth/oauth/token", json = auth_data)
-    access_token_dict = json.loads(auth_request.text)
-    headers = {"Authorization":"Bearer " + access_token_dict["access_token"]}
+
+    auth_request = requests.post(BASE_URL + "/auth/oauth/token", json=auth_data)
+    access_token_dict = auth_request.json()
+
+    headers = {"Authorization": "Bearer " + access_token_dict["access_token"]}
     identity_request = requests.get(BASE_URL + "/auth/api/v1/users/me", headers=headers)
-    identity_dict = json.loads(identity_request.text)
+    identity_dict = identity_request.json()
     profile_dict = identity_dict["response"]["profile"]
-    response = "You have successfully logged in for " + profile_dict["first_name"] + " " + profile_dict["last_name"]
+
+    response = "You have successfully logged in for {} {}".format(
+        profile_dict["first_name"], profile_dict["last_name"])
     return response
+
 
 if __name__ == "__main__":
     app.run(ssl_context='adhoc')
